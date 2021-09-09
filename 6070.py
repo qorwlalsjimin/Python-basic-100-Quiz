166a546056a8c901bbdbf39a04a1a87bb177366e
Baek Jimin <s2104@e-mirim.hs.kr>
2021-09-10 08:22:48 +0900

    [기초-조건/선택실행구조] 월 입력받아 계절 출력하기

---
+++ 6070.py
@@ -0,0 +1,16 @@
+n = int(input())
+if n//3==1 :
+    print("spring")
+else :
+    if n//3==2 :
+        print("summer")
+    else :
+        if n//3==3 :
+            print("fall")
+        else :
+            print("winter")
+            
+#12, 1, 2
+#3, 4, 5
+#6, 7, 8
+#9, 10, 11
